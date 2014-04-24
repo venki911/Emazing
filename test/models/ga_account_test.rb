432c9d038da203e038ebb31d456a4420b9c1297f
Tomaž Žlender <tomaz.zlender@gmail.com>
2014-04-24 19:39:00 +0200

    dopolnil kodo za testirat povezavo z google analytics

--- test/models/ga_account_test.rb
+++ test/models/ga_account_test.rb
@@ -1,31 +1,35 @@
 require 'test_helper'
+require "google/api_client"
 
 class GaAccountTest < ActiveSupport::TestCase
   test "preveri ali deluje povezava z google analytics" do
-  	@client = Google::APIClient.new(application_name: 'Emazing orodje',application_version: '1.0.0')
+    @client = Google::APIClient.new(application_name: 'Emazing orodje',application_version: '1.0.0')
 
-		key = Google::APIClient::KeyUtils.load_from_pkcs12(Rails.application.secrets.google_account_certificate_path, 'notasecret')
-		@client.authorization = Signet::OAuth2::Client.new(
-		  :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-		  :scope => 'https://www.googleapis.com/auth/analytics',
-		  :audience => 'https://accounts.google.com/o/oauth2/token',
-		  :issuer => '188678993551@developer.gserviceaccount.com',
-		  :signing_key => key)
+    key = Google::APIClient::KeyUtils.load_from_pkcs12(Rails.application.secrets.google_account_certificate_path, 'notasecret')
+    @client.authorization = Signet::OAuth2::Client.new(
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+      scope: 'https://www.googleapis.com/auth/analytics',
+      audience: 'https://accounts.google.com/o/oauth2/token',
+      issuer: '649902570085@developer.gserviceaccount.com',
+      signing_key: key
+    )
 
-		@client.authorization.fetch_access_token!
-		@analytics = @client.discovered_api('analytics', 'v3')
+    @client.authorization.fetch_access_token!
+    @analytics = @client.discovered_api('analytics', 'v3')
 
-		startDate = DateTime.now.prev_month.strftime("%Y-%m-%d")
-		endDate = DateTime.now.strftime("%Y-%m-%d")
-		profileID = GaAccount.first.account_id
+    startDate = DateTime.now.prev_month.strftime("%Y-%m-%d")
+    endDate = DateTime.now.strftime("%Y-%m-%d")
+    profileID = '85241985'
 
-		@client.execute(api_method: @analytics.data.ga.get, :parameters => {
-			'ids' => "ga:" + profileID, 
-			'start-date' => startDate,
-			'end-date' => endDate,
-			'dimensions' => "ga:day,ga:month",
-			'metrics' => "ga:visits",
-			'sort' => "ga:month,ga:day"
-		})
+    @client.execute(api_method: @analytics.data.ga.get, :parameters => {
+      'ids' => "ga:" + profileID, 
+      'start-date' => startDate,
+      'end-date' => endDate,
+      'dimensions' => "ga:day,ga:month",
+      'metrics' => "ga:visits",
+      'sort' => "ga:month,ga:day"
+    })
+
+    @client.execute(api_method: @analytics.management.accounts.list)
   end
 end
