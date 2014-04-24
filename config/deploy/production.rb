b4329669c78589859947d32025c72cad65e58f77
Tomaž Žlender <tomaz.zlender@gmail.com>
2014-04-24 16:09:37 +0200

    dopolnil capistrano nastavitve, spremenil verzijo net-ssh gem-a, ker ima privzeta bug

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deployer@178.32.143.131}
+role :web, %w{deployer@178.32.143.131}
+role :db,  %w{deployer@178.32.143.131}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '178.32.143.131', user: 'deployer', roles: %w{web app db}
 
 
 # Custom SSH Options
