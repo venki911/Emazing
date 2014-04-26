1d8c6f24ba7bb51250f26613c8e05ac7cb1bae03
Tomaž Žlender <tomaz.zlender@gmail.com>
2014-04-26 17:03:30 +0200

    samodejno ponovno zazeni unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,8 +45,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute 'sudo /etc/init.d/unicorn-emazing restart'
     end
   end
 
